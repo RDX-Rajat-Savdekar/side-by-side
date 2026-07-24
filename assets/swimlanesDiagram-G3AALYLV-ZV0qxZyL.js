a749033bdeacf139d209e7aad07969e3b0ba97f2
Rajat Savdekar <156324870+RDX-Rajat-Savdekar@users.noreply.github.com>
2026-07-24 15:31:51 -0700

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-ZV0qxZyL.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-Cc4t4wGD.js";import{_ as a}from"./index-B2jlUOqH.js";import"./chunk-5VM5RSS4-Bm0ZnXIA.js";import"./chunk-XXDRQBXY-DdbGzzKi.js";import"./chunk-VR4S4FIN-ChmiZmAl.js";import"./chunk-32BRIVSS-DYl0ahjg.js";import"./channel-4CZ4Ax3w.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
